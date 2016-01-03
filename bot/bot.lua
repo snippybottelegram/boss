e18326aed53106055198df7e57938570f56097fa
snippybottelegram <mohammad.moghimi.me@gmail.com>
2016-01-03 13:52:21 +0330

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -228,7 +228,7 @@ function create_config( )
       "youtube",
       "media_handler",
       "moderation"},
-    sudo_users = {omidtarh,70480064},
+    sudo_users = {147191022},
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'}
   }
